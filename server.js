1255c56850acf34b9cdf9354ce2e0cc41672c739
David Evans <davidje13@users.noreply.github.com>
2018-11-18 13:34:45 +0000

    Add dedicated 2-person chambers which do not require queues, so can have more chambers

--- server.js
+++ server.js
@@ -22,17 +22,27 @@ if (DOMAINS === '') {
 // (MAX_QUEUE_DATA + HEADERS_MAX_LENGTH) * CHAMBER_MAX_CONNECTIONS * MAX_CHAMBERS
 // + some overhead from data structures
 // Memory usage will typically be much lower (unless explicitly attacked)
-// The values below result in ~0.5GB peak memory usage
+// The values below result in ~0.25GB peak memory usage
 
 const echoChamber = new EchoChamber('/', DOMAINS, {
 	MAX_QUEUE_ITEMS: 1024,
 	MAX_QUEUE_DATA: 16 * 1024,
 	HEADERS_MAX_LENGTH: 1024,
 	CHAMBER_MAX_CONNECTIONS: 64,
+	MAX_CHAMBERS: 256,
+});
+
+// 2-person echo chambers do not require a queue, so we can support more of them
+const p2EchoChamber = new EchoChamber('/p2/', DOMAINS, {
+	MAX_QUEUE_ITEMS: 0,
+	MAX_QUEUE_DATA: 0,
+	HEADERS_MAX_LENGTH: 1024,
+	CHAMBER_MAX_CONNECTIONS: 2,
 	MAX_CHAMBERS: 512,
 });
 
 new WebSocketServer()
+	.addHandler(p2EchoChamber)
 	.addHandler(echoChamber)
 	.listen(PORT, HOSTNAME)
 	.then((server) => server.printListeningInfo(process.stdout));
